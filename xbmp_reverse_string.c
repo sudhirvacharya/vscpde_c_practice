592e93634c63abfda1a32fe4916c9e25e3864bcf
sudhirvacharya <sudheerpaniyur@gmail.com>
2026-03-24 04:16:51 +0000

    added

--- xbmp_reverse_string.c
+++ xbmp_reverse_string.c
@@ -21,13 +21,32 @@ void recur_reverse_str(char *ptr)
     }
    
 }
+
+void reverse12(char *ptr, int n)
+{
+    int left =0;
+    int right=n-1;
+    char tmp;
+
+    while(left < right)
+    {
+        tmp= ptr[left];
+        ptr[left]=ptr[right];
+        ptr[right]=tmp;
+        left++;
+        right;;
+    }
+}
 int main()
 {
     char str[]="india";
     int n=sizeof(str)/sizeof(str[0]);
     //reverse_str(str, n-1);
-    recur_reverse_str(str);
-    
-    
+    //recur_reverse_str(str);
+
+    reverse12(str, n-1);
+    printf("%s",str);
+
+       
     return 0;
 }
