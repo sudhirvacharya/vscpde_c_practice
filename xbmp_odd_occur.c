fceb5f6f6e5646548d63d869435f5eb0b50587cb
sudhirvacharya <sudheerpaniyur@gmail.com>
2026-03-24 03:08:34 +0000

    added

--- xbmp_odd_occur.c
+++ xbmp_odd_occur.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*  this works only for odd number of repeation 1,3,5,7,9...
 
+00->0
+01->1
+10->0
+11->0
+
+//if the odd input, output willl be high
+000->0
+001->1
+010->1
+011->0
+
+*/
 int main()
 {
     int array[]={2,2,3,3,4,4,4,1,1};
