ccfbbcda28d064bddd75f81817acc4353f41cea9
sudhirvacharya <sudheerpaniyur@gmail.com>
2026-04-10 03:20:44 +0000

    added

--- xbmp_swap_nibble.c
+++ xbmp_swap_nibble.c
@@ -4,9 +4,12 @@
 int main()
 {
     unsigned char var =0xAB;
+    unsigned int var1 =0x12345678;
     var = ( (var & 0xF0) >> 4) | ( (var & 0x0F) << 4);
+    var1=  (var1 & 0xffff0000  ) >> 16  |   (var1 & 0x00000ffff ) << 16;
 
     printf(" swap nibble %x \n", var);
+    printf(" swap nibble %x \n", var1);
 
     return 0;
 }
