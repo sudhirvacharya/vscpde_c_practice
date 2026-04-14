a1c2f7dc59d163fecc079acf196155748d267d5e
sudhirvacharya <sudheerpaniyur@gmail.com>
2026-04-14 03:31:13 +0000

    added

--- memory_layout.c
+++ memory_layout.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
+
+    	int a = 0x02;          // an integer
+	    int *ptr = &a;         // pointer to int
+	    int **ptr_ptr = &ptr;  // pointer to pointer to int
 
         int a_stack;
     static int b_bss;
     static int c_init=8;
     volatile register int d_reg=10;
-    char *ptr="ro_data";
+  //  char *ptr="ro_data";
     char *buff_ptr=malloc(10);
 
     printf("a_stack adress=%p \t",&a_stack);
@@ -33,6 +38,18 @@ int main()
 
 
 
+
+	    printf("a: %d\n", a);
+	    printf("*ptr: %d\n", *ptr);
+	    printf("**ptr_ptr: %d\n", **ptr_ptr);
+
+	    // Print addresses
+	    printf("\nAddresses:\n");
+	    printf("&a: %p\n", (void*)&a);
+	    printf("ptr (address of a): %p\n", (void*)ptr);
+	    printf("&ptr: %p\n", (void*)&ptr);
+	    printf("ptr_ptr (address of ptr): %p\n", (void*)ptr_ptr);
+	    printf("&ptr_ptr: %p\n", (void*)&ptr_ptr);
 
     return 0;
 }
