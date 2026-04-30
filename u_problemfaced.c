91db57e0f5872d53a06c70dbe78803ae65b43ae5
sudheerpaniyur <sudheerpaniyur@proton.me>
2026-04-30 10:57:22 +0000

    problems

---
+++ u_problemfaced.c
@@ -0,0 +1,6 @@
+
+//problem faced in my project and how resolved
+1. mcu was reseting due to divide by zero
+    found reeset reason which line causing, in that line written /0 
+
+2.due to adding nvm block, rcc clock reated test case failing, in nvm integrity check testcase
