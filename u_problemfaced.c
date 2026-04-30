1a234af9c9e9fdc0886f1a546fe20e7a8f5e2b79
sudheerpaniyur <sudheerpaniyur@proton.me>
2026-04-30 11:03:41 +0000

    problems

--- u_problemfaced.c
+++ u_problemfaced.c
@@ -4,3 +4,6 @@
     found reeset reason which line causing, in that line written /0 
 
 2.due to adding nvm block, rcc clock reated test case failing, in nvm integrity check testcase
+
+3. sw buil failing dude to no space in flash region, 
+    solutuion:adjusted memory in linker from from other region, updated adress
