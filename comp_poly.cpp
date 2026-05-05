0741240aea0a1f3f9b8016a07e2f2767ff86c9c4
sudheerpaniyur <sudheerpaniyur@proton.me>
2026-05-05 02:31:51 +0000

    update

--- comp_poly.cpp
+++ comp_poly.cpp
@@ -1,26 +1,30 @@
 #include <stdio.h>
+
+//compile and run cmd
+//g++ comp_poly.cpp 
+
 //compile time polymorphism
 //fucntion over lload also called polymorphism
 //same name diffrent behavoir at compile time
 
 // this is called fucnton over loading
-void print( int a)
+void add( int a, int b)
 {
 
-printf( "%d \n",a);
+printf( "int:->%d \n", a+b);
 }
 
-void print (float a)
+void add (float a, float b)
 {
-printf( "%f \n",a);
+printf( "float:->%f \n", a+b);
 
 }
 
 int main()
 {
 
-print(2);
-print(4.3f);
+add(2, 3);
+add(4.3f, 4.3f);
 
 return 0;
 }
