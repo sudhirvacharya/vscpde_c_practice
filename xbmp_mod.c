a56a5d7468879c8be2d98117c3bf7c6a80044668
sudheerpaniyur <sudheerpaniyur@proton.me>
2026-06-10 01:03:58 +0000

    updated code

---
+++ xbmp_mod.c
@@ -0,0 +1,18 @@
+/* ==========================================================
+   MODULO (%) TRICKS WORTH KNOWING
+   ==========================================================
+
+   Pattern             Meaning                         Example
+   ----------------------------------------------------------------
+   n % 2 == 0          Even number                     8 % 2 == 0
+
+   n % 2 != 0          Odd number                      7 % 2 != 0
+
+   n % 10              Last digit of n                 1234 % 10 = 4
+
+   n % 100             Last two digits of n            1234 % 100 = 34
+
+   (i + 1) % n         Circular wrap-around index      (4+1)%5 = 0
+
+   n % k == 0          n is a multiple of k            15 % 5 == 0
+*/
