039d06c514c467b5756428215bbddc48a89cf3fa
sudhirvacharya <sudheerpaniyur@gmail.com>
2026-06-30 03:18:55 +0000

    updated

--- array_of_ptr.c
+++ array_of_ptr.c
@@ -2,14 +2,23 @@
 
 int main() {
     int a = 10, b = 20, c = 30;
-    int *arr[3];   // array of 3 int pointers
+    int array[]={1,2,3,4};
+    int *ptr[3];   // array of 3 int pointers
 
-    arr[0] = &a;
-    arr[1] = &b;
-    arr[2] = &c;
+    ptr[0] = &a;
+    ptr[1] = &b;
+    ptr[2] = &c;
 
     for(int i=0; i<3; i++) {
-        printf("Value at arr[%d] = %d\n", i, *arr[i]);
+        printf("Value at arr[%d] = %d\n", i, *ptr[i]);
     }
+
+    for (int i=0;i<3 ;i++)
+    {
+        ptr[i]=&array[i]
+           printf("Value at arr[%d] = %d\n", i, *ptr[i]);
+    }
+
+
     return 0;
 }
